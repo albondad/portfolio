b0c37fc794b8515d4c2961a3b368c5c221224db3
Al Bondad <albondad@gmail.com>
2021-10-19 21:20:17 -0500

    Set up NavigationBar component

---
+++ packages/website/common/NavigationBar/NavigationBar.styles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const NavigationBar =  styled.div`
+`;
+
+export const NavigationBarMain = styled.div`
+    display: flex;
+    margin-right: auto;
+    margin-left: auto;
+    max-width: 100rem;
+    padding: 0.75rem 4rem;
+`
+
+export const NavigationBarMainBrand =  styled.div`
+    flex-grow: 1;
+    font-size: 2rem;
+    font-weight: 700;
+`
+
+export const NavigationBarMainLinks =  styled.div`
+    display: flex;
+`
+
+export const NavigationBarMainLinksItem =  styled.div`
+    cursor: pointer;
+    margin-left: 3rem;
+    font-weight: 700;
+    font-size: 1.5rem;
+`
+
