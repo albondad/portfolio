9ffa2517a594cc143e940fd93019948b24456ae8
Al Bondad <albondad@gmail.com>
2022-09-07 22:05:28 -0500

    Update constrained content sizing

--- packages/website/components/constrained-content/constrained-content.styled-components.tsx
+++ packages/website/components/constrained-content/constrained-content.styled-components.tsx
@@ -5,12 +5,12 @@ export const StyledBox = styled(Box)`
   & {
     margin-left: auto;
     margin-right: auto;
-    max-width: calc(100rem + 2rem * 2);
+    max-width: calc(87.5rem + 2rem * 2);
     padding-left: 2rem;
     padding-right: 2rem;
 
     @media screen and (min-width: ${(props) => props.theme.largeBreakpoint}) {
-      max-width: calc(100rem + 4rem * 2);
+      max-width: calc(87.5rem + 4rem * 2);
       padding-left: 4rem;
       padding-right: 4rem;
     }
