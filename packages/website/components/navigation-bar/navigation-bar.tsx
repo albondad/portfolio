ea830578ed0edfb006a70ce258a6697d6fe5e23f
Al Bondad <albondad@gmail.com>
2022-09-14 14:30:26 -0500

    Update navigationb ar message

--- packages/website/components/navigation-bar/navigation-bar.tsx
+++ packages/website/components/navigation-bar/navigation-bar.tsx
@@ -20,7 +20,7 @@ export const NavigationBar = ({
                 <ConstrainedContent>
                     Thanks for dropping by! I{"'"}m still in the middle of
                     developing albondad.com so you can expect some bug fixes and
-                    design changes. On the other hand, feel free to explore!
+                    design changes in the future.
                 </ConstrainedContent>
             </Box>
             <ConstrainedContent className="NavigationBar-constrainedContent">
